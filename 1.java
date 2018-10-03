9a9ba64e09d61c4bd133b2c0cf975f405ddd910d
zerodayman <43405940+zerodayman@users.noreply.github.com>
2018-10-03 09:27:32 +0700

    Add files via upload

---
+++ 1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+
+public class Main 
+
+{
+    
+    public static void main(String[] args) throws Exception
+
+{
+  
+   
+        
+       
+ System.out.println("Hello , World");
+   
+
+ }
+
+
+}
